f3f985a5ed2062e9288682ac30ff1f5d6a69053a
RodrDevs80 <rodr26707@gmail.com>
2023-01-05 06:29:43 -0300

    juego del ahorcado version pixel parctica

---
+++ sweetAlerte.js
@@ -0,0 +1,12 @@
+Swal.fire({
+    title: "BIENVENIDO!!!",
+    text:"¿Estas listo para jugar?",
+    icon:"question",
+    confirmButtonText:"JUGAR",
+    footer:"Utilice el teclado para ingresar las letras!",
+    background:"rgba(138,221,45,0.7)",
+    grow:"fullscreen",
+    imageUrl:"juego.jpg",
+    imageWidth:"20%"
+
+});
